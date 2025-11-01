542f002c5b70a88cf378375ab5365bdd46b5f93b
ThangVS <thangvuong2512@gmail.com>
2025-11-01 13:09:22 +0700

    collection page

--- Blastopia_Web/Blastopia_Web/Controllers/HomeController.cs
+++ Blastopia_Web/Blastopia_Web/Controllers/HomeController.cs
@@ -28,5 +28,25 @@ namespace Blastopia_Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        // Dán 3 hàm này vào file Controllers/HomeController.cs
+
+
+        public IActionResult Community()
+        {
+            return View();
+        }
+
+        public IActionResult Terms()
+        {
+            return View();
+        }
+        public IActionResult Story()
+        {
+            return View();
+        }
+        public IActionResult Collection()
+        {
+            return View();
+        }
     }
 }
